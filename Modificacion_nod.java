48fdb1350bb75e8970776a9041490ff73bfd6934
Brezly404 <89997036+Brezly404@users.noreply.github.com>
2021-11-14 16:29:32 -0600

    Add files via upload
    Tomando como base el ejercicio anterior lista simplemente enlazada v1.2,
    
    agregar la opción eliminar al menu.
    Este método eliminará el primer o el último nodo ingresado. Ustedes deciden cual utilizar

--- Modificacion_nod.java
+++ Modificacion_nod.java
@@ -1,13 +1,17 @@
+package lista_simple_1_3;
+
 
 import java.util.Scanner;
 
 public class Modificacion_nod {
 
     static class Node {
+
         String name;
         Node next;
     }
-    static void dump (String msg, Node topNode) {
+
+    static void dump(String msg, Node topNode) {
         System.out.print(msg + " ");
 
         while (topNode != null) {
@@ -24,82 +28,58 @@ public class Modificacion_nod {
         int opt = 0;
         Node top = null;
         top = new Node();
-        
+
         do {
-           
-            System.out.println("ingrese la opción requerida:\n"
-            +" 1. Agregar nodo inicial\n"
-            +" 2. Agregar nodo atras del inicial\n"
-            +" 3. Agregar nodo delante de incial\n"
-            +" 4. Agregar nodo intermedio\n"
-            +" 5. Mostrar lista\n"
-            +" 6. Detener\n");
+
+            System.out.println("\ningrese la opción requerida:\n"
+            +"1. Agregar nodo inicial\n"
+            +"2. muestra\n" 
+            +"3. eliminar nodo incial\n"
+            +"4. buscar nodo\n"
+            +"5. fin\n");
 
             switch (opt = rd.nextInt()) {
 
-            case 1:
+                case 1:
 
-                System.out.println("ingresa contenido de nodo incial:");
-                top.name = sc.nextLine();
-                top.next = null;
+                    System.out.print("ingresa contenido de nodo incial: ");
+                    top.name = sc.nextLine();
+                    top.next = null;
 
-                break;
+                    Node temp;
 
-            case 2:
-                Node temp;
+                    temp = new Node();
+                    System.out.print("ingresa contenido de nodo anterior a inicial: ");
+                    temp.name = sc.nextLine();
+                    temp.next = top;
+                    top = temp;
 
-                temp = new Node();
-                System.out.println("ingresa contenido de nodo anterior a inicial");
-                temp.name = sc.nextLine();
-                temp.next = top;
-                top = temp;
-                break;
+                    break;
+                    
+                case 2:
 
-            case 3:
-                temp = new Node();
-                System.out.println("ingresa contenido de nodo posterior a inicial");
-                temp.name = sc.nextLine();
-                temp.next = null;
+                    if (top != null) {
 
-                Node temp2;
+                        System.out.println("La lista esta vacia");
+                    } else {
+                        dump("Vista del nodo\n", top);
+                    }
 
-                temp2 = top;
+                    break;
 
-                while (temp2.next != null) {
-                    temp2 = temp2.next;
-                }
+                case 3:
 
-                temp2.next = temp;
-
-
-                break;
-            case 4:
-                
-            //No logre hacer que se insertara entre un rango de valores
-            //solo que se introdujera de manera automatica
-            
-                temp = new Node();
-                System.out.println("ingresa contenido de nodo intermedio");
-                temp.name = sc.nextLine();
-
-                temp2 = top;
-
-                while (temp2.name.equals(top.name) == false) {
-                    temp2 = temp2.next;
-                }
-
-                temp.next = temp2.next;
-                temp2.next = temp;
-
-                break;
-            case 5:
-
-                dump("Vista del nodo\n", top);
-                break;
+                    if (top == null) {
+                        System.out.println("No hay valores que eliminar\n");
+                    } else {
+                        top = top.next;
+                        dump("nodo eliminado: ", top);
+                        break;
+                    }
 
             }
 
-        } while (opt != 6);
+        } while (opt != 5);
 //By: brezly
     }
 
